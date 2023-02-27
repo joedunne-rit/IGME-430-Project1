4f4c5a83ded7e8401fef540c4a6ec3f3ad2fc710
Joseph Dunne <jmd1494@rit.edu>
2023-02-27 13:20:37 -0500

    Working on necessary methods for spellbook responses/client-side code for making spell list

--- src/spellbookResponses.js
+++ src/spellbookResponses.js
@@ -1,1 +1,28 @@
-// const users = {};
+const users = {};
+
+const addSpellList = (request, response, spellListData) => {
+    const userName = spellListData.userName;
+    const listName = spellListData.listName;
+    const spellList = spellListData.spells;
+    
+    //If username and list name already exist, just update spells
+    if (users[userName][listName]){
+        users[userName][listName] = spellList;
+        //Send info to POST response method
+        return;
+    }
+    //If username exists but list name doesnt, add new list name and spells
+    if (users[userName]){
+        users[userName][listName] = spellList;
+        //send info
+        return;
+    }
+    //Otherwise, create new user, spell list name, and write spells
+    users[userName] = {
+        [listName]: spellList
+    };
+    //send info
+    return;
+}
+
+//const loadSpellList
